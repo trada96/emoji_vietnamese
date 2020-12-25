85aadaf78477d6a2567b354ab844bb5a0932f22d
callmetuananh <tuananhhd5596@gmail.com>
2020-12-25 11:33:37 +0700

    add emoji vietnamese

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+"""
+Setup script for emoji
+"""
+
+
+from codecs import open
+import os
+
+from setuptools import setup
+
+
+with open('README.rst', encoding='utf-8') as f:
+    readme_content = f.read().strip()
+
+
+author = email = source = version = None
+with open(os.path.join('emoji', '__init__.py'), encoding='utf-8') as f:
+    for line in f:
+        if line.strip().startswith('__version__'):
+            version = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__author__'):
+            author = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__email__'):
+            email = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__source__'):
+            source = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif None not in (version, author, email, source):
+            break
+
+setup(
+    name='emoji_vietnamese',
+    author=author,
+    author_email=email,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    description="Emoji for Python convert vietnamesse",
+    keywords=['emoji_vietnamese'],
+    extras_require={
+        'dev': [
+            'pytest',
+            'coverage',
+            'coveralls'
+        ]
+    },
+    include_package_data=True,
+    license="New BSD",
+    long_description=readme_content,
+    packages=['emoji_vietnamese'],
+    url=source,
+    version=version,
+    zip_safe=True,
+)
---
+++ tests/test_core.py
@@ -0,0 +1,79 @@
+# -*- coding: UTF-8 -*-
+
+
+"""
+Unittests for emoji.core
+"""
+
+
+from __future__ import unicode_literals
+
+import emoji
+
+
+def test_emojize_name_only():
+    for name in emoji.EMOJI_UNICODE.keys():
+        actual = emoji.emojize(name, False)
+        expected = emoji.EMOJI_UNICODE[name]
+        assert expected == actual, "%s != %s" % (expected, actual)
+
+
+def test_emojize_complicated_string():
+    # A bunch of emoji's with UTF-8 strings to make sure the regex expression is functioning
+    name_code = {
+        ':flag_for_Ceuta_&_Melilla:': u'\U0001F1EA\U0001F1E6',
+        ':flag_for_St._Barthélemy:': u'\U0001F1E7\U0001F1F1',
+        ':flag_for_Côte_d’Ivoire:': u'\U0001F1E8\U0001F1EE',
+        ':flag_for_Åland_Islands:': u'\U0001F1E6\U0001F1FD',
+        ':flag_for_São_Tomé_&_Príncipe:': u'\U0001F1F8\U0001F1F9',
+        ':flag_for_Curaçao:': u'\U0001F1E8\U0001F1FC'
+    }
+    string = ' complicated! '.join(list(name_code.keys()))
+    actual = emoji.emojize(string, False)
+    expected = string
+    for name, code in name_code.items():
+        expected = expected.replace(name, code)
+    expected = emoji.emojize(actual, False)
+    assert expected == actual, "%s != %s" % (expected, actual)
+
+
+def test_emojize_invalid_emoji():
+    string = '__---___--Invalid__--__-Name'
+    assert emoji.emojize(string, False) == string
+
+
+def test_alias():
+    # When use_aliases=False aliases should be passed through untouched
+    assert emoji.emojize(':soccer:', use_aliases=False) == ':soccer:'
+    assert emoji.emojize(':soccer:', use_aliases=True) == emoji.EMOJI_ALIAS_UNICODE[':soccer:']
+
+
+def test_invalid_alias():
+    # Invalid aliases should be passed through untouched
+    assert emoji.emojize(':tester:', use_aliases=True) == ':tester:'
+
+
+def test_demojize_name_only():
+    for name in emoji.EMOJI_UNICODE.keys():
+        oneway = emoji.emojize(name, False)
+        roundtrip = emoji.demojize(oneway)
+        assert name == roundtrip, "%s != %s" % (name, roundtrip)
+
+
+def test_demojize_complicated_string():
+    constructed = u"testing :baby::emoji_modifier_fitzpatrick_type-3: with :eyes: :eyes::eyes: modifiers :baby::emoji_modifier_fitzpatrick_type-5: to symbols ヒㇿ"
+    emojid = emoji.emojize(constructed)
+    destructed = emoji.demojize(emojid)
+    assert constructed == destructed, "%s != %s" % (constructed, destructed)
+
+
+def test_emoji_lis():
+    assert emoji.emoji_lis("Hi, I am fine. 😁") == [{'location': 15, 'emoji': '😁'}]
+    assert emoji.emoji_lis("Hi") == []
+    assert emoji.emoji_lis("Hello 🇫🇷👌") == [{'emoji': '🇫🇷', 'location': 6}, {'emoji': '👌', 'location': 8}]
+
+
+def test_emoji_count():
+    assert emoji.emoji_count("Hi, I am fine. 😁") == 1
+    assert emoji.emoji_count("Hi") == 0
+    assert emoji.emoji_count("Hello 🇫🇷👌") == 2
